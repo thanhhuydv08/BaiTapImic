3871367a75a6d54a1069eb1a701436926bf67e93
thanhhuydv08 <thanhhuydv08@gmail.com>
2018-06-08 19:34:00 +0700

    bai tap thuc hanh 1

---
+++ huy/src/member01/nguyenthanhhuy/ThucHanh02_PrinterTamGiac.java
@@ -0,0 +1,41 @@
+package member01.nguyenthanhhuy;
+
+import java.util.Scanner;
+
+public class ThucHanh02_PrinterTamGiac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		Scanner sc = new Scanner(System.in);
+		System.out.printf(" Nhap chieu cao cua tam giac : ");
+		int rowHeight = sc.nextInt();
+		System.out.println();
+		/// tách ra so lẽ in khác số chẵn in khác nhé
+	if (rowHeight%2!=0) {
+		PrinterOldNumber(rowHeight);
+	}else PrinterEvenNumber(rowHeight);
+	}
+public static void PrinterOldNumber(int h) {
+	for (int i=0; i<h; i++) {
+		for ( int j=0; j< h+i; j++) {
+			if(j>=(h-i-1)&&(i+j)%2==0) {
+				System.out.print("*");
+			}else System.out.print(" ");
+		
+		}
+		System.out.println();
+	}
+}
+public static void PrinterEvenNumber(int h) {
+	for (int i=0; i<h; i++) {
+		for ( int j=0; j< h+i; j++) {
+			if(j>=(h-i-1)&&(i+j)%2!=0) {
+				System.out.print("*");
+			}else System.out.print(" ");	
+		}
+		System.out.println();
+	}
+}
+
+}
+
